601d26701ee9ffba84c7b74d68c82d7f12110b86
mason <masonaesposito@gmail.com>
2018-08-17 08:10:32 -0700

    first commit

---
+++ server/datasources.local.js
@@ -0,0 +1,14 @@
+module.exports =
+{
+    "db": {
+      "name": "db",
+      "connector": "memory"
+    },
+    "MongoDB": {
+      "name": "MongoDB",
+      "localStorage": "",
+      "file": "",
+      "connector": "memory"
+    }
+  }
+  
